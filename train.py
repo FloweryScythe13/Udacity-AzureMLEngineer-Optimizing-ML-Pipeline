aed1013784621ec297afd08ccd47231e703da1d5
FloweryScythe13 <brichett13@gmail.com>
2022-09-08 12:12:17 -0500

    Fixing an error in train.py, committing nb changes

--- train.py
+++ train.py
@@ -61,8 +61,7 @@ def main():
 
     # TODO: Split data into train and test sets.
 
-    x_train, x_test, y_train, y_test = train_test_split(
-...     x, y, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
